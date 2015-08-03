6d6c2e62e1bb71d7dcc39b91e141b59ae21d34cf
David Kudera <kudera.d@gmail.com>
2015-08-03 11:26:20 +0200

    Latte: add macro no-grid-has-paginator [closes #14]

--- src/Latte/Macros.php
+++ src/Latte/Macros.php
@@ -29,6 +29,7 @@ class Macros extends MacroSet
 		$me->addMacro('no-grid-views-as', '', [$me, 'macroNoGridViewsAs']);
 		$me->addMacro('no-grid-empty', [$me, 'macroNoGridEmpty'], '}');
 		$me->addMacro('no-grid-not-empty', [$me, 'macroNoGridNotEmpty'], '}');
+		$me->addMacro('no-grid-has-paginator', [$me, 'macroNoGridHasPaginator'], '}');
 
 		return $me;
 	}
@@ -106,6 +107,21 @@ class Macros extends MacroSet
 		}
 
 		return $writer->write('if ($_noGrid->getCount() > 0) {');
+	}
+
+
+	/**
+	 * @param \Latte\MacroNode $node
+	 * @param \Latte\PhpWriter $writer
+	 * @return string
+	 */
+	public function macroNoGridHasPaginator(MacroNode $node, PhpWriter $writer)
+	{
+		if (!$this->findParentMacro($node, 'no-grid')) {
+			throw new MacroDefinitionException('Macro no-grid-not-empty must be inside of no-grid macro.');
+		}
+
+		return $writer->write('if ($_noGrid->getTotalCount() > $_noGrid->getCount()) {');
 	}
 
 
